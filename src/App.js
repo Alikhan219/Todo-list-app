b0b4239ddf7ff7d39f02336e06dfed7e0834c1ab
unknown <merabhai786786@gmail.com>
2020-10-12 05:30:50 -0700

    creating the App component

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
+import React from 'react'
 
+const App=()=>{
+
+return(
+    <>
+    <div className= "main_div">
+<div className="center_div">
+   <br />
+   <h1> ToDo List </h1>
+   <br />
+   <input type="text" placeholder="Add an Item" />
+   <button > + </button>
+
+   <ol>
+       <li>buy apple</li>
+   </ol>
+</div>
+    </div>        
+    </>
+)
+
+
+
+}
+
+
+export default App;
